ee5b1a886e014a2e11b118ab9fbe7a6942794911
DESKTOP-D0JHBK5\renan <renancidale@hotmail.com>
2018-07-25 10:51:40 +0200

    implemented more features to the crawler

--- filmesbrasil_cr.py
+++ filmesbrasil_cr.py
@@ -18,6 +18,7 @@ list_of_channels = ['Canal Brasil',
  ] 
 
 list_of_links = [ 'http://canalbrasil.globo.com/programacao.html',
+
 'http://megapix.globo.com/',
 'https://br.cinemax.tv/schedule',
 'http://www.boxbrazil.tv.br/prime-box-brazil-grade-de-programacao/',
@@ -32,38 +33,56 @@ url = (list_of_links[0])
 #Open the url with Selenium, since the data that I need is being rendered through Javascript,
 #I have to use Selenium. 
 browser.get(url)
-xpath = '//*[@id="area-background"]/main/div/div[2]/div[1]/div/ul/div/div/div[8]'
-delay = 3
-try:
-    myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
-    print("Page is ready!")
-except TimeoutException:
-    print("Loading took too much time!")
-# get the page_source.
-myElem.click() 
+#number that will control the days in the page. starting with 7 because it's the first day 
 
-time.sleep(2)
-html_source = browser.page_source
-# close the browser
+#store all the objects created
+main_objs = []
+#Using Xpath because I can iterate through the items. 
+
+for i in range(7,11):
+	number = i
+	xpath = f'//*[@id="area-background"]/main/div/div[2]/div[1]/div/ul/div/div/div[{number}]'
+	delay = 3
+	try:
+	    myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
+	    print("Page is ready!")
+	except TimeoutException:
+	    print("Loading took too much time!")
+	# get the page_source.
+	myElem.click() 
+
+	time.sleep(2)
+	html_source = browser.page_source
+	# close the browser
+	# use the page source on Beautiful just like if it was done through requests.
+	soup = bs.BeautifulSoup(html_source, "lxml")
+
+	#Getting information about the movie
+	main_div = soup.find('div', class_='horas')
+	items = main_div.find_all('div', class_='texto')
+
+	my_text = []
+	class Text():
+
+		def __init__(self, subtitulo, complemento, classificacao,year,origin_country):
+			self.subtitulo = subtitulo
+			self.complemento = complemento
+			self.classificacao = classificacao
+			self.year = year
+			self.origin_country = origin_country
+
+
+
+	for i in items:
+		my_text.append(Text(i.h5.text.strip(), 
+							i.p.text.strip(),
+							i.find('span', class_='classificacao').text.split(),
+							i.find('span', class_='ano').text.split(),
+							i.find('span', class_='pais').text.split()
+							))
+	main_objs.append(my_text)
 browser.quit()
-# use the page source on Beautiful just like if it was done through requests.
-soup = bs.BeautifulSoup(html_source, "lxml")
-
-#Getting information about the movie
-main_div = soup.find('div', class_='horas')
-items = main_div.find_all('div', class_='texto')
-
-my_text = []
-class Text():
-
-	def __init__(self, subtitulo, complemento):
-		self.subtitulo = subtitulo
-		self.complemento = complemento
-
-for i in items:
-	my_text.append(Text(i.h5.text.strip(), i.p.text.strip()))
-
-print(my_text[0].subtitulo)
+print(main_objs[0][1].subtitulo)
 # get class titulo to get either a movie/cinemao @@ whatever da fuck that shit is.
 # get class subtitulo to get the titulo
 # get class complemento for resumo

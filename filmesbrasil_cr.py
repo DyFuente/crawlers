f6d1861b457c94272f090cf25c664583891c1f33
DESKTOP-D0JHBK5\renan <renancidale@hotmail.com>
2018-07-25 23:30:19 +0200

    finished scraping CanalBrasil

--- filmesbrasil_cr.py
+++ filmesbrasil_cr.py
@@ -12,7 +12,7 @@ import json
 browser = webdriver.Chrome("C:/Users/renan/Tutorial/chromedriver.exe")
 
 browser.maximize_window()
-
+browser.execute_script("document.body.style.zoom='100'")
 list_of_channels = ['Canal Brasil',
  'Megapix',
  'Cinemax',
@@ -52,41 +52,68 @@ class Text():
 			self.origin_country = origin_country
 
 my_text = []
-for i in range(7,11):
+for i in range(7,21):
 	number = i
-	xpath = f'//*[@id="area-background"]/main/div/div[2]/div[1]/div/ul/div/div/div[{number}]'
-	delay = 3
-	try:
-	    myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
-	    print("Page is ready!")
-	except TimeoutException:
-	    print("Loading took too much time!")
-	# get the page_source.
-	myElem.click() 
+	next_button_xpath = '//span[@class="seta proximo"]//span[@class="icone"]'
+	if(number < 11):
+		xpath = f'//*[@id="area-background"]/main/div/div[2]/div[1]/div/ul/div/div/div[{number}]'
+		delay = 3
+		try:
+		    myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, xpath)))
+		    print("Page is ready!")
+		except TimeoutException:
+		    print("Loading took too much time!")
+		# get the page_source.
+		myElem.click() 
 
-	time.sleep(2)
-	html_source = browser.page_source
-	# close the browser
-	# use the page source on Beautiful just like if it was done through requests.
-	soup = bs.BeautifulSoup(html_source, "lxml")
-	#Getting information about the movie
-	items = soup.find_all('div', class_='episodio')
-	for i in items:
-		b = Text(i.find('h5', class_='subtitulo').text.strip(), 
-				i.find('p', class_='complemento').text.strip(),
-				i.find('span', class_='classificacao').text.split(),
-				i.find('span', class_='ano').text.split(),
-				i.find('span', class_='pais').text.split()
-							)
-		my_text.append(b)
+		time.sleep(2)
+		html_source = browser.page_source
+		# close the browser
+		# use the page source on Beautiful just like if it was done through requests.
+		soup = bs.BeautifulSoup(html_source, "lxml")
+		#Getting information about the movie
+		items = soup.find_all('div', class_='episodio')
+		for i in items:
+			b = Text(i.find('h5', class_='subtitulo').text.strip(), 
+					i.find('p', class_='complemento').text.strip(),
+					i.find('span', class_='classificacao').text.split(),
+					i.find('span', class_='ano').text.split(),
+					i.find('span', class_='pais').text.split()
+								)
+			my_text.append(b)
+	else:
+		next_button_xpath = '//span[@class="seta proximo"]'
+		_xpath= f"//div[@class='owl-wrapper']//div[{number}]"
+		browser.find_element_by_xpath(next_button_xpath).click()
+		time.sleep(5)
+		browser.find_element_by_xpath(_xpath).click()
+		html_source = browser.page_source
+		# close the browser
+		# use the page source on Beautiful just like if it was done through requests.
+		soup = bs.BeautifulSoup(html_source, "lxml")
+		#Getting information about the movie
+		items = soup.find_all('div', class_='episodio')
+		for i in items:
+			b = Text(i.find('h5', class_='subtitulo').text.strip(), 
+					i.find('p', class_='complemento').text.strip(),
+					i.find('span', class_='classificacao').text.split(),
+					i.find('span', class_='ano').text.split(),
+					i.find('span', class_='pais').text.split()
+								)
+			my_text.append(b)
+browser.quit()
+my_file = jsonpickle.encode(my_text)
+
 for i in my_text:
 	print(i.subtitulo)
 	print(i.complemento)
 	print(i.classificacao)
 	print(i.year)
 	print(i.origin_country)
-	print('---------------------------------')
-browser.quit()
+	print("-------------------------")
+with open('text.json', 'w') as f:
+	json.dump(my_file, f, indent=2, ensure_ascii=False)
+
 
 #for i, c in enumerate(my_text):
 #	print(c[i].classificacao)

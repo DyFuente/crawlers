ac1990320d16b1b9b1274c982918b23e0dc47596
DESKTOP-D0JHBK5\renan <renancidale@hotmail.com>
2018-07-25 21:57:38 +0200

    fixed some mistakes with the crawler

--- filmesbrasil_cr.py
+++ filmesbrasil_cr.py
@@ -6,8 +6,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException  
 import time
+import jsonpickle
+import json
 
 browser = webdriver.Chrome("C:/Users/renan/Tutorial/chromedriver.exe")
+
+browser.maximize_window()
 
 list_of_channels = ['Canal Brasil',
  'Megapix',
@@ -18,7 +22,6 @@ list_of_channels = ['Canal Brasil',
  ] 
 
 list_of_links = [ 'http://canalbrasil.globo.com/programacao.html',
-
 'http://megapix.globo.com/',
 'https://br.cinemax.tv/schedule',
 'http://www.boxbrazil.tv.br/prime-box-brazil-grade-de-programacao/',
@@ -37,8 +40,18 @@ browser.get(url)
 
 #store all the objects created
 main_objs = []
-#Using Xpath because I can iterate through the items. 
 
+#Using Xpath because I can iterate through the items. 
+class Text():
+
+		def __init__(self, subtitulo, complemento, classificacao,year,origin_country):
+			self.subtitulo = subtitulo
+			self.complemento = complemento
+			self.classificacao = classificacao
+			self.year = year
+			self.origin_country = origin_country
+
+my_text = []
 for i in range(7,11):
 	number = i
 	xpath = f'//*[@id="area-background"]/main/div/div[2]/div[1]/div/ul/div/div/div[{number}]'
@@ -56,40 +69,33 @@ for i in range(7,11):
 	# close the browser
 	# use the page source on Beautiful just like if it was done through requests.
 	soup = bs.BeautifulSoup(html_source, "lxml")
-
 	#Getting information about the movie
-	main_div = soup.find('div', class_='horas')
-	items = main_div.find_all('div', class_='texto')
-
-	my_text = []
-	class Text():
-
-		def __init__(self, subtitulo, complemento, classificacao,year,origin_country):
-			self.subtitulo = subtitulo
-			self.complemento = complemento
-			self.classificacao = classificacao
-			self.year = year
-			self.origin_country = origin_country
-
-
-
+	items = soup.find_all('div', class_='episodio')
 	for i in items:
-		my_text.append(Text(i.h5.text.strip(), 
-							i.p.text.strip(),
-							i.find('span', class_='classificacao').text.split(),
-							i.find('span', class_='ano').text.split(),
-							i.find('span', class_='pais').text.split()
-							))
-	main_objs.append(my_text)
+		b = Text(i.find('h5', class_='subtitulo').text.strip(), 
+				i.find('p', class_='complemento').text.strip(),
+				i.find('span', class_='classificacao').text.split(),
+				i.find('span', class_='ano').text.split(),
+				i.find('span', class_='pais').text.split()
+							)
+		my_text.append(b)
+for i in my_text:
+	print(i.subtitulo)
+	print(i.complemento)
+	print(i.classificacao)
+	print(i.year)
+	print(i.origin_country)
+	print('---------------------------------')
 browser.quit()
-print(main_objs[0][1].subtitulo)
+
+#for i, c in enumerate(my_text):
+#	print(c[i].classificacao)
 # get class titulo to get either a movie/cinemao @@ whatever da fuck that shit is.
 # get class subtitulo to get the titulo
 # get class complemento for resumo
 # get class classificacao for age restriction
 # get class ano for year of the movie
 # get class pais to get the origin of the movie
-
 
 
 
